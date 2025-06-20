23519a3ce819e5d660921e193d3532f8c4b70377
ringovandenhende <vandenhende.ringo@gmail.com>
2025-06-20 13:59:00 +0200

    enable transactionManagement + change compose

--- budget-buddy-back/src/main/java/be/yorian/budgetbuddy/BudgetbuddyBackApplication.java
+++ budget-buddy-back/src/main/java/be/yorian/budgetbuddy/BudgetbuddyBackApplication.java
@@ -2,8 +2,10 @@ package be.yorian.budgetbuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class BudgetbuddyBackApplication {
 
     public static void main(String[] args) {
